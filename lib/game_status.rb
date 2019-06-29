dcc022a8cbf38c5da5ec1d03a5f2e155bb247a56
Sanima Shrestha <saneemastha@gmail.com>
2019-06-29 14:52:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,31 +4,34 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]]
+WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]]
+
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |win_array|
+    WIN_COMBINATIONS.detect do |win_array|
     board[win_array[0]] == board[win_array[1]] &&
     board[win_array[1]] == board[win_array[2]] && 
     position_taken?(board,win_array[0])
   end
 end
-
+  
 def full?(board)
-  board.all?{ |element| element == "X" || element == "O"}
-end
-
-def draw?(board)
-  !won?(board) && full?(board)
-end
-
-def over?(board)
-  draw?(board) || full?(board) || won?(board)
-end
-
-def winner(board)
-  if player = won?(board)
-    return board[player.first]
+  board.all? do |element|
+    element!=""&& element=="X" || element=="O"
   end
 end
 
+def draw?(board)
+  !won?(board)|| full?(board)
+  
+end
+
+def over?(board)
+  won?(board)||draw?(board)||full?(board) ||board==""
+end
+
+def winner?(board)
+  if player==won?(board)
+    return player[player.first]
+  end
+end
