76f30f00a7a7f1ad096e39c3842810ac16a336cc
Sanima Shrestha <saneemastha@gmail.com>
2019-06-29 14:48:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,6 +8,12 @@ WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8
 
 
 def won?(board)
+    WIN_COMBINATIONS.detect do |win_array|
+    board[win_array[0]] == board[win_array[1]] &&
+    board[win_array[1]] == board[win_array[2]] && 
+    position_taken?(board,win_array[0])
+  end
+end
   
 end
 def full?(board)
